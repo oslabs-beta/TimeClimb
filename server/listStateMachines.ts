9cadc83fd14908adf550baedd6f289614a84521f
Andrew Mott <andrew@andrewmott.com>
2024-10-08 16:50:10 -0400

    Adds DescribeStateMachineCommand for getting Step Function ASL json

--- server/listStateMachines.ts
+++ server/listStateMachines.ts
@@ -4,9 +4,9 @@ import {
   ListStateMachinesCommand,
   ListStateMachineVersionsCommand,
   ListStateMachineVersionsCommandOutput,
+  DescribeStateMachineCommand,
 } from "@aws-sdk/client-sfn";
 import { fromEnv } from "@aws-sdk/credential-providers";
-console.log("process.env", process.env);
 
 /**
  * Reads access token from these environment variables:
@@ -21,22 +21,52 @@ const sfn = new SFNClient({
 const listStateMachines = new ListStateMachinesCommand();
 const response = await sfn.send(listStateMachines);
 console.log("listStateMachines response", response);
+
+/**
+ * get the detailed implementations of a state machine
+ */
+if (response.stateMachines && response.stateMachines[0].stateMachineArn) {
+  getStateMachineDetails(response.stateMachines[0].stateMachineArn);
+}
+async function getStateMachineDetails(
+  stateMachineArn: string
+): Promise<undefined> {
+  const describeStateMachine = new DescribeStateMachineCommand({
+    stateMachineArn,
+  });
+
+  const response = await sfn.send(describeStateMachine);
+  console.log("getStateMachineDetails reponse", response);
+
+  return undefined;
+}
+
 /**
  * Get state machine version information for each state machine
  */
-const stateMachineVersions: ListStateMachineVersionsCommandOutput[] = [];
+async function getStateMachineVersions(
+  response: ListStateMachineVersionsCommandOutput
+): undefined {
+  const stateMachineVersions: ListStateMachineVersionsCommandOutput[] = [];
 
-if (response.stateMachines) {
-  for (const stateMachine of response.stateMachines) {
-    const input = {
-      stateMachineArn: stateMachine.stateMachineArn,
-    };
-    const listStateMachineVerions = new ListStateMachineVersionsCommand(input);
-    const response = await sfn.send(listStateMachineVerions);
-    if (response.stateMachineVersions) {
-      console.log("stateMachienVersionObject:", response.stateMachineVersions);
+  if (response.stateMachines) {
+    for (const stateMachine of response.stateMachines) {
+      const input = {
+        stateMachineArn: stateMachine.stateMachineArn,
+      };
+      const listStateMachineVerions = new ListStateMachineVersionsCommand(
+        input
+      );
+      const response = await sfn.send(listStateMachineVerions);
+      if (response.stateMachineVersions) {
+        console.log(
+          "stateMachienVersionObject:",
+          response.stateMachineVersions
+        );
+      }
+      stateMachineVersions.push(response);
     }
-    stateMachineVersions.push(response);
   }
+  console.log("listStateMachienVersions", stateMachineVersions);
+  return undefined;
 }
-console.log("listStateMachienVersions", stateMachineVersions);
