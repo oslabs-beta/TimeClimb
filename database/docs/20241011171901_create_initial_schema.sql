e4439aa01af9ca0e6c13a8b9499bf67155803b13
Andrew Mott <andrew@andrewmott.com>
2024-10-11 16:20:04 -0400

    Creates tables for initial schema

--- database/docs/20241011171901_create_initial_schema.sql
+++ database/docs/20241011171901_create_initial_schema.sql
@@ -13,11 +13,63 @@ create table "step_functions" (
   "has_versions" boolean not null, 
   "parent_id" integer);
 
-alter table "step_functions" add constraint 
-  "step_functions_parent_id_foreign" 
+alter table "step_functions" add constraint "step_functions_parent_id_foreign" 
   foreign key ("parent_id") 
   references "step_functions" ("step_function_id");
 
+create table "steps" (
+  "step_id" serial primary key, 
+  "step_function_id" integer not null, 
+  "name" text not null, 
+  "type" varchar(20) not null, 
+  "comment" text);
+    
+alter table "steps" add constraint "steps_step_function_id_foreign" 
+  foreign key ("step_function_id") 
+  references "step_functions" ("step_function_id");
+
+create table "step_latencies" (
+  "latency_id" bigserial primary key, 
+  "step_id" integer not null, 
+  "average" double precision not null, 
+  "executions" bigint not null, 
+  "start_time" timestamptz not null, 
+  "end_time" timestamptz not null);
+
+alter table "step_latencies" add constraint "step_latencies_step_id_foreign" 
+  foreign key ("step_id") 
+  references "steps" ("step_id"); 
+
+create table "step_function_latencies" (
+  "latency_id" bigserial primary key, 
+  "step_function_id" integer not null, 
+  "average" double precision not null, 
+  "executions" bigint not null, 
+  "start_time" timestamptz not null, 
+  "end_time" timestamptz not null);
+
+alter table "step_function_latencies" 
+  add constraint "step_function_latencies_step_function_id_foreign" 
+    foreign key ("step_function_id") 
+    references "step_functions" ("step_function_id");
+
+create table "step_function_monitoring" (
+  "monitor_id" serial primary key, 
+  "step_function_id" integer not null, 
+  "newest_update" timestamptz, 
+  "oldest_update" timestamptz, 
+  "start_time" timestamptz, 
+  "end_time" timestamptz, 
+  "active" boolean not null);
+
+alter table "step_function_monitoring" 
+  add constraint "step_function_monitoring_step_function_id_foreign" 
+    foreign key ("step_function_id") 
+    references "step_functions" ("step_function_id");
 
 -- down method sql output
-drop table if exists "step_functions"
+drop table if exists "step_functions";
+drop table if exists "steps";
+drop table if exists "step_latencies";
+drop table if exists "step_function_latencies";
+drop table if exists "step_function_monitoring";
--- database/migrations/20241011171901_create_initial_schema.ts
+++ database/migrations/20241011171901_create_initial_schema.ts
@@ -1,3 +1,4 @@
+import { table } from "console";
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
@@ -21,10 +22,79 @@ export async function up(knex: Knex): Promise<void> {
     })
     .toSQL();
   console.log("create step_functions", sql);
+
+  sql = await knex.schema
+    .createTable("steps", (table) => {
+      table.increments("step_id").notNullable();
+      table.integer("step_function_id").notNullable();
+      table.text("name").notNullable();
+      table.string("type", 20).notNullable();
+      table.text("comment");
+      table
+        .foreign("step_function_id")
+        .references("step_function_id")
+        .inTable("step_functions");
+    })
+    .toSQL();
+
+  sql = await knex.schema
+    .createTable("step_latencies", (table) => {
+      table.bigIncrements("latency_id").notNullable();
+      table.integer("step_id").notNullable();
+      table.double("average").notNullable();
+      table.bigInteger("executions").notNullable();
+      table.timestamp("start_time", { useTz: true }).notNullable();
+      table.timestamp("end_time", { useTz: true }).notNullable();
+      table.foreign("step_id").references("step_id").inTable("steps");
+    })
+    .toSQL();
+  console.log("create step_latencies table", sql);
+
+  sql = await knex.schema
+    .createTable("step_function_latencies", (table) => {
+      table.bigIncrements("latency_id").notNullable();
+      table.integer("step_function_id").notNullable();
+      table.double("average").notNullable();
+      table.bigInteger("executions").notNullable();
+      table.timestamp("start_time", { useTz: true }).notNullable();
+      table.timestamp("end_time", { useTz: true }).notNullable();
+      table
+        .foreign("step_function_id")
+        .references("step_function_id")
+        .inTable("step_functions");
+    })
+    .toSQL();
+  console.log("create step_function_latencies table", sql);
+
+  sql = await knex.schema
+    .createTable("step_function_monitoring", (table) => {
+      table.increments("monitor_id").notNullable();
+      table.integer("step_function_id").notNullable();
+      table.timestamp("newest_update", { useTz: true });
+      table.timestamp("oldest_update", { useTz: true });
+      table.timestamp("start_time", { useTz: true });
+      table.timestamp("end_time", { useTz: true });
+      table.boolean("active").notNullable();
+      table
+        .foreign("step_function_id")
+        .references("step_function_id")
+        .inTable("step_functions");
+    })
+    .toSQL();
+
+  console.log("create step_function_monitoring table", sql);
 }
 
 export async function down(knex: Knex): Promise<void> {
-  const sql = await knex.schema.dropTableIfExists("step_functions").toSQL();
+  let sql = await knex.schema.dropTableIfExists("step_functions").toSQL();
+  console.log("drop sql:", sql);
+  sql = await knex.schema.dropTableIfExists("steps").toSQL();
+  console.log("drop sql:", sql);
+  sql = await knex.schema.dropTableIfExists("step_latencies").toSQL();
+  console.log("drop sql:", sql);
+  sql = await knex.schema.dropTableIfExists("step_function_latencies").toSQL();
+  console.log("drop sql:", sql);
+  sql = await knex.schema.dropTableIfExists("step_function_monitoring").toSQL();
   console.log("drop sql:", sql);
   // await knex.schema.dropTableIfExists("step_functions");
   // await knex.schema.dropTableIfExists("step_functions");
