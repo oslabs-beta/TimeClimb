1805ec817e925b6135765af2bfce055221a2f2c9
Sharon <Sharonpatterson808@gmail.com>
2024-10-16 11:48:24 -0400

    testing outline

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
       <Provider store={store}>
         {/* <LoginForm /> */}
         {/* <SignUpForm />  */}
+        <LandingPage/>
         <DetailedView />
       </Provider>
     </>
