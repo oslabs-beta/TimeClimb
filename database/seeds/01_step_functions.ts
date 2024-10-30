cdea098e2cfc2562a64d04998a6d94f4e43fb1a8
Sharon <Sharonpatterson808@gmail.com>
2024-10-30 13:04:40 -0400

    fixed migration bug

--- database/seeds/01_step_functions.ts
+++ database/seeds/01_step_functions.ts
@@ -3,7 +3,7 @@ import { Knex } from "knex";
 import definitions from "./utils/step-function-definitions";
 import { StepFunctionsTable } from "../../server/models/types";
 
-// export async function seed(knex: Knex): Promise<void> {
+export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
   await knex("step_functions").del();
 
